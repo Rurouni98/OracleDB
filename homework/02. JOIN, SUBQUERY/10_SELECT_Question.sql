9c9842a010bf0aa3228ad9dc218bfb89ab3975d9
Rurouni98 <byungju98@gmail.com>
2024-05-29 19:35:54 +0900

    24.05.29 김병주 강의 후 업데이트

--- homework/02. JOIN, SUBQUERY/10_SELECT_Question.sql
+++ homework/02. JOIN, SUBQUERY/10_SELECT_Question.sql
@@ -1,24 +1,8 @@
 --1) 학생중에 동명이인을 검색한다
-SELECT DISTINCT ST.SNAME
-	FROM STUDENT ST
-	JOIN STUDENT STM
-	  ON ST.SNAME = STM.SNAME
-	 AND ST.SNO != STM.SNO;
+
 
 --2) 전체 교수 명단과 교수가 담당하는 과목의 이름을 학과 순으로 검색한다
-SELECT P.PNAME
-	 , C.CNAME
-	FROM PROFESSOR P
-	LEFT JOIN COURSE C
-	  ON P.PNO = C.PNO
-	ORDER BY P.SECTION;
 
---3) 이번 학기 등록된 모든 과목 정보와 담당 교수를 학점 순으로 검색한다
-SELECT C.*
-	 , P.PNAME
-	FROM COURSE C
-	JOIN PROFESSOR P
-	  ON C.PNO = P.PNO
-	JOIN SCORE SC
-	  ON C.CNO = SC.CNO
-	ORDER BY C.ST_NUM DESC;
+
+--3) 이번 학기 등록된 모든 과목과 담당 교수의 학점 순으로 검색한다
+
